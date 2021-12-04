b6450d13cfcf5042e49f4043381e102e0eaae4a8
anshul jain <55848572+shadowknight08@users.noreply.github.com>
2021-12-04 23:33:42 +0530

    AR application

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Horse Anatomy AR/Assets/Scripts/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+
+   
+    public int currentanim = 0;
+    public int Currentanim
+    {
+        get
+        {
+            return currentanim;
+           
+        }
+
+        set
+        {
+            currentanim = value;
+            if (currentanim == 1)
+            {
+                label.SetActive(true);
+            }
+            else
+            {
+                label.SetActive(false);
+            }
+        }
+    }
+    public Animator anim;
+    public GameObject label;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void Next()
+    {
+        if (Currentanim< 2)
+        {
+            Currentanim++;
+            anim.SetInteger("nextaim", currentanim);
+        }
+       
+        
+    }
+
+    public void Previous()
+    {
+        if(Currentanim > 0)
+        {
+            Currentanim--;
+            anim.SetInteger("nextaim", currentanim);
+        }
+       
+    }
+}
